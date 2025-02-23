a71150f4cf6fa87e8bdbed53cd49e48e337f1467
zham <kopalka.xo@gmail.com>
2025-02-23 21:10:18 +0500

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request, pk):
+def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -23,7 +23,7 @@ def post_new(request, pk):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm(instance=post)
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
