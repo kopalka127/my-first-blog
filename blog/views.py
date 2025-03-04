1cd6ecb3c9fa338dd2aacc87f9e58d4731f99df2
zham <kopalka.xo@gmail.com>
2025-03-04 16:49:37 +0500

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,41 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
-from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
-from django.shortcuts import redirect
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+def news_view(request):
+    url = "https://yk.kz/"
+    response = requests.get(url)
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    articles_data = []
 
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        articles = soup.find_all("li", attrs={"data-news-item": True})
+
+        for article in articles:
+            title_tag = article.find("div", class_="name")
+            title_text = title_tag.text.strip() if title_tag else "Без названия"
+
+            link_tag = article.find("a")
+            link_href = link_tag["href"] if link_tag and link_tag.has_attr("href") else "#"
+            link_href = urljoin(url, link_href)
+
+            articles_data.append({"title": title_text, "link": link_href})
+
     else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+        print(f"Ошибка загрузки страницы: {response.status_code}")
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, "blog/news.html", {"articles": articles_data})
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import news_view  
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path("admin/", admin.site.urls),
+    path("", news_view, name="home"),  
+    path("news/", news_view, name="news_list"),
 ]
+
+
+
+
+#path("", post_list, name="home"),
+
